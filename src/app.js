73870362e4a83041258a538d1cecac97ef83f758
Tyler <tgrande@my.bcit.ca>
2022-04-27 19:59:19 -0700

    cleaned the js code, added a stripping for img file titles

--- src/app.js
+++ src/app.js
@@ -3,62 +3,18 @@ async function makeFetchtoBackEndToGetData() {
     const json = await response.json()
     return json
   }
-  
-
-
-
-  
-  async function addSomeElementsToThePage() {
-    const listOfMovies = await makeFetchtoBackEndToGetData()
-    return listOfMovies
-  }
-  
-
-
-
 
 const allMovies = document.getElementById("display_movies")
-
 let idTracker = 0
 
-// function createMovieElement(listOfMovies){
-//   const movie = document.createElement("div")
-//   const img = document.createElement("img")
-//   const name = document.createElement("p")
-//   movie.id = idTracker
-//   idTracker = idTracker + 1
-//   img.src = 'img/rushhour.jfif'
-//   movie.append(img)
-//   name.textContent = listOfMovies[0].title
-//   movie.append(name)
-//   allMovies.append(movie)
-// }
-
-
-// const listOfMovies = addSomeElementsToThePage()
-// createMovieElement(listOfMovies)
-// console.log(listOfMovies)
-
-
-
-
 async function main() {
-    // await makeFetchtoBackEndToGetData()
     const listOfMovies = await makeFetchtoBackEndToGetData()
     await displayAllMovies(listOfMovies)
-    // await viewDataInMovieList(listOfMovies)
-    // console.log(listOfMovies)
-  }
 
-// function viewDataInMovieList(listOfMovies) {
-//     for (let information of listOfMovies){
-//         console.log(information)
-//     }
-// }
+  }
 
 function displayAllMovies(listOfMovies){
     for (let numberOfMovies of listOfMovies) {
-        // createMovieElement(listOfMovies)
         const titleTag = document.createElement('p');
         const objectTag = document.createElement('p');
         const ratingTag = document.createElement('p');
@@ -71,8 +27,9 @@ function displayAllMovies(listOfMovies){
         for (let object in numberOfMovies) {
             console.log(object)
             if ( object === 'title' ) {
+                noSpecialCharacters = numberOfMovies.title.replace(/[^a-zA-Z0-9 ]/g, '')
                 titleTag.textContent = numberOfMovies.title
-                imgTag.src = 'img/' + numberOfMovies.title + '.jfif'
+                imgTag.src = 'img/' + noSpecialCharacters + '.jfif'
                 movieDiv.append(titleTag)
                 movieDiv.append(imgTag)
             }
